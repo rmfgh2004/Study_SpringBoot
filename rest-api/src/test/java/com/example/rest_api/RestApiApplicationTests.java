ce02ed1059514a31a76a6d3a94b2b71d8fb7c2a0
rmfgh2004 <76586854+rmfgh2004@users.noreply.github.com>
2025-03-15 02:09:30 +0000

    Object Mapper 공부

--- rest-api/src/test/java/com/example/rest_api/RestApiApplicationTests.java
+++ rest-api/src/test/java/com/example/rest_api/RestApiApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.rest_api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.rest_api.model.UserRequest;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 class RestApiApplicationTests {
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JsonProcessingException {
+		var user = new UserRequest();
+		user.setUserName("홍길동");
+		user.setUserAge(10);
+		user.setEmail("gmail.com");
+		user.setIsKorean(true);
+		
+		var json = objectMapper.writeValueAsString(user);
+		System.out.println(json);
+
+		var dto = objectMapper.readValue(json, UserRequest.class);
+		System.out.println(dto);
 	}
 
 }
