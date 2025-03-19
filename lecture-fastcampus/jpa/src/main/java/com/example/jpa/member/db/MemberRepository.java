8475dad1a70e84b007bd8bcd6c9c5e0fca084168
rmfgh2004 <76586854+rmfgh2004@users.noreply.github.com>
2025-03-19 13:32:02 +0000

    파일 수정

--- lecture-fastcampus/jpa/src/main/java/com/example/jpa/member/db/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jpa.member.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-
-    
-    
-}
--- lecture-fastcampus/simple-board/src/main/java/com/example/simple_board/crud/Converter.java
+++
@@ -1,9 +0,0 @@
-package com.example.simple_board.crud;
-
-public interface Converter<DTO, ENTITY> {
-
-    DTO toDto(ENTITY entity);
-
-    ENTITY toEntity(DTO dto);
-    
-}
